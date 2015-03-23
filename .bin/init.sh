462c7b28545acc638a8cd2176e42243b454397a9
sugarshin <shinsugar@gmail.com>
2015-03-23 20:16:13 +0900

    add coffeelint

--- .bin/init.sh
+++ .bin/init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-npm i -D browserify coffee-script coffeeify derequire espower-coffee jsdom mocha power-assert uglify-js
+npm i -D browserify coffee-script coffeeify coffeelint derequire espower-coffee jsdom mocha power-assert uglify-js
 node ./.bin/init.js $1
 mkdir build lib
 rm -rf .git .bin
